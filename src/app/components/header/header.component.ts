15fd39942e92b266e0cb9dca591ba16afe8c6360
Wekesa <wekesa350@gmail.com>
2021-11-26 17:34:34 +0300

    Limiting components base on routes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,18 +1,20 @@
 import {Component, OnInit} from '@angular/core';
 import {Subscription} from "rxjs";
 import {UiService} from "../../services/ui.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
   showAddTask: boolean = false;
   subscription?: Subscription;
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     this.subscription = this.uiService.onToggle().subscribe(
       (showAddTask: boolean) => {
         this.showAddTask = showAddTask;
@@ -25,5 +27,9 @@ export class HeaderComponent implements OnInit {
 
   toggleAddTask() {
     this.uiService.toggleAddTask()
+  }
+
+  hasRoute(route: string){
+    return this.router.url=== route;
   }
 }
